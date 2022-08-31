rmartin16/qbittorrent-api

qBittorrent v4.5.0 Support  - Add support for torrents/export  - Add support for transfer/setSpeedLimitsMode

qbittorrentapi/torrents.py
@@ -21,6 +21,7 @@ from qbittorrentapi.decorators import check_for_raise
 from qbittorrentapi.decorators import endpoint_introduced
 from qbittorrentapi.decorators import handle_hashes
 from qbittorrentapi.decorators import login_required
+from qbittorrentapi.decorators import response_bytes
 from qbittorrentapi.decorators import response_json
 from qbittorrentapi.decorators import response_text
 from qbittorrentapi.definitions import APINames
@@ -387,6 +388,10 @@ class TorrentDictionary(Dictionary):
         self._client.torrents_remove_tags(
             torrent_hashes=self._torrent_hash, tags=tags, **kwargs
         )
+
+    def export(self, **kwargs):
+        """Implements :meth:`~TorrentsAPIMixIn.export`"""
+        return self._client.torrents_export(torrent_hash=self._torrent_hash, **kwargs)
 
 
 class TorrentPropertiesDictionary(Dictionary):
@@ -1616,6 +1621,25 @@ class TorrentsAPIMixIn(AppAPIMixIn):
                     "This endpoint is available starting in Web API 2.7."
                 ),
             )
+
+    @handle_hashes
+    @endpoint_introduced("2.8.14", "torrents/export")
+    @response_bytes
+    @login_required
+    def torrents_export(self, torrent_hash=None, **kwargs):
+        """
+        Export a .torrent file for the torrent.
+
+        :raises NotFound404Error: torrent not found
+        :raises Conflict409Error: unable to export .torrent file
+
+        :param torrent_hash: hash for torrent
+        :return: bytes .torrent file
+        """
+        data = {"hash": torrent_hash}
+        return self._post(
+            _name=APINames.Torrents, _method="export", data=data, **kwargs
+        )
 
     ##########################################################################
     # MULTIPLE TORRENT ENDPOINTS

tests/test_transfer.py
@@ -18,11 +18,25 @@ def test_speed_limits_mode(client):
     assert client.transfer.speed_limits_mode in ("0", "1")
 
     original_mode = client.transfer.speed_limits_mode
+    client.transfer_set_speed_limits_mode()
+    assert client.transfer.speed_limits_mode != original_mode
+    original_mode = client.transfer.speed_limits_mode
+    client.transfer.set_speed_limits_mode()
+    assert client.transfer.speed_limits_mode != original_mode
+
+    original_mode = client.transfer.speed_limits_mode
     client.transfer_toggle_speed_limits_mode()
     assert client.transfer.speed_limits_mode != original_mode
     original_mode = client.transfer.speed_limits_mode
     client.transfer.toggle_speed_limits_mode()
     assert client.transfer.speed_limits_mode != original_mode
+
+    if client.transfer.speed_limits_mode == "0":
+        client.transfer_set_speed_limits_mode(intended_state=True)
+        assert client.transfer.speed_limits_mode != "0"
+    if client.transfer.speed_limits_mode == "1":
+        client.transfer_set_speed_limits_mode(intended_state=False)
+        assert client.transfer.speed_limits_mode != "1"
 
     if client.transfer.speed_limits_mode == "0":
         client.transfer_toggle_speed_limits_mode(intended_state=True)
@@ -44,9 +58,6 @@ def test_speed_limits_mode(client):
     if client.transfer.speedLimitsMode == "1":
         client.transfer.speedLimitsMode = False
         assert client.transfer.speedLimitsMode != "1"
-
-    client.transfer_toggle_speed_limits_mode(intended_state=False)
-    assert client.transfer.speed_limits_mode != "1"
 
 
 def test_download_limit(client):

aprabhq/qbkro

qbittorrentapi/torrents.py
@@ -21,6 +21,7 @@ from qbittorrentapi.decorators import check_for_raise
 from qbittorrentapi.decorators import endpoint_introduced
 from qbittorrentapi.decorators import handle_hashes
 from qbittorrentapi.decorators import login_required
+from qbittorrentapi.decorators import response_bytes
 from qbittorrentapi.decorators import response_json
 from qbittorrentapi.decorators import response_text
 from qbittorrentapi.definitions import APINames
@@ -387,6 +388,10 @@ class TorrentDictionary(Dictionary):
         self._client.torrents_remove_tags(
             torrent_hashes=self._torrent_hash, tags=tags, **kwargs
         )
+
+    def export(self, **kwargs):
+        """Implements :meth:`~TorrentsAPIMixIn.torrents_export`"""
+        return self._client.torrents_export(torrent_hash=self._torrent_hash, **kwargs)
 
 
 class TorrentPropertiesDictionary(Dictionary):
@@ -1616,6 +1621,25 @@ class TorrentsAPIMixIn(AppAPIMixIn):
                     "This endpoint is available starting in Web API 2.7."
                 ),
             )
+
+    @handle_hashes
+    @endpoint_introduced("2.8.14", "torrents/export")
+    @response_bytes
+    @login_required
+    def torrents_export(self, torrent_hash=None, **kwargs):
+        """
+        Export a .torrent file for the torrent.
+
+        :raises NotFound404Error: torrent not found
+        :raises Conflict409Error: unable to export .torrent file
+
+        :param torrent_hash: hash for torrent
+        :return: bytes .torrent file
+        """
+        data = {"hash": torrent_hash}
+        return self._post(
+            _name=APINames.Torrents, _method="export", data=data, **kwargs
+        )
 
     ##########################################################################
     # MULTIPLE TORRENT ENDPOINTS

tests/test_transfer.py
@@ -18,35 +18,42 @@ def test_speed_limits_mode(client):
     assert client.transfer.speed_limits_mode in {"0", "1"}
 
     original_mode = client.transfer.speed_limits_mode
+    client.transfer_set_speed_limits_mode()
+    assert client.transfer.speed_limits_mode != original_mode
+    original_mode = client.transfer.speed_limits_mode
+    client.transfer.set_speed_limits_mode()
+    assert client.transfer.speed_limits_mode != original_mode
+
+    original_mode = client.transfer.speed_limits_mode
     client.transfer_toggle_speed_limits_mode()
     assert client.transfer.speed_limits_mode != original_mode
+    client.transfer_toggle_speed_limits_mode()
+    assert client.transfer.speed_limits_mode == original_mode
     original_mode = client.transfer.speed_limits_mode
     client.transfer.toggle_speed_limits_mode()
     assert client.transfer.speed_limits_mode != original_mode
+    client.transfer.toggle_speed_limits_mode()
+    assert client.transfer.speed_limits_mode == original_mode
 
-    if client.transfer.speed_limits_mode == "0":
-        client.transfer_toggle_speed_limits_mode(intended_state=True)
-        assert client.transfer.speed_limits_mode != "0"
-    if client.transfer.speed_limits_mode == "1":
-        client.transfer_toggle_speed_limits_mode(intended_state=False)
-        assert client.transfer.speed_limits_mode != "1"
+    client.transfer_set_speed_limits_mode(intended_state=True)
+    assert client.transfer.speed_limits_mode != "0"
+    client.transfer_setSpeedLimitsMode(intended_state=False)
+    assert client.transfer.speed_limits_mode != "1"
 
-    if client.transfer.speed_limits_mode == "0":
-        client.transfer.speed_limits_mode = True
-        assert client.transfer.speed_limits_mode != "0"
-    if client.transfer.speed_limits_mode == "1":
-        client.transfer.speed_limits_mode = False
-        assert client.transfer.speed_limits_mode != "1"
-
-    if client.transfer.speedLimitsMode == "0":
-        client.transfer.speedLimitsMode = True
-        assert client.transfer.speedLimitsMode != "0"
-    if client.transfer.speedLimitsMode == "1":
-        client.transfer.speedLimitsMode = False
-        assert client.transfer.speedLimitsMode != "1"
-
+    client.transfer_toggle_speed_limits_mode(intended_state=True)
+    assert client.transfer.speed_limits_mode != "0"
     client.transfer_toggle_speed_limits_mode(intended_state=False)
     assert client.transfer.speed_limits_mode != "1"
+
+    client.transfer.speed_limits_mode = True
+    assert client.transfer.speed_limits_mode != "0"
+    client.transfer.speed_limits_mode = False
+    assert client.transfer.speed_limits_mode != "1"
+
+    client.transfer.speedLimitsMode = True
+    assert client.transfer.speedLimitsMode != "0"
+    client.transfer.speedLimitsMode = False
+    assert client.transfer.speedLimitsMode != "1"
 
 
 def test_download_limit(client):